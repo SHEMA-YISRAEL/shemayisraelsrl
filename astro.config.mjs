dd8b7da7353daacba3cc917d79e8ae723976d826
kevinzapata23 <shema.sistemas.op3@gmail.com>
2025-09-23 17:50:40 -0400

    change: username to domain public

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://shema-yisrael.github.io', // username github
-  base: 'shemayisraelsrl', // name repo
+  site: 'https://shemayisraelsrl.com', // username github or domain
+  base: '/', // name repo or root route
 });
