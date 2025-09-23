205b291c12eb8ada66d8210ec27e4f81e25f834d
kevinzapata23 <shema.sistemas.op3@gmail.com>
2025-09-23 16:33:48 -0400

    fix: username astro config and workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://SHEMA-YISRAEL.github.io', // username github
+  site: 'https://shema-yisrael.github.io', // username github
   base: 'shemayisraelsrl', // name repo
 });
