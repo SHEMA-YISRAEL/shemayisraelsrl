9d537385a5fa8ae32918b5fe3ccb47624d09d6c2
kevinzapata23 <shema.sistemas.op3@gmail.com>
2025-09-23 15:57:03 -0400

    add: workflow to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://SHEMA-YISRAEL.github.io', // username github
+  base: 'shemayisraelsrl', // name repo
+});
